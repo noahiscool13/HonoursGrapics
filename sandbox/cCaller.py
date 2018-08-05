bf9eddc81821105d8922b98c8bb5a94933aaaad2
noahiscool13 <noah@gamestoplay.nl>
2018-08-05 13:17:06 +0200

    Add pybind benchmark

--- sandbox/cCaller.py
+++ sandbox/cCaller.py
@@ -1,1 +1,29 @@
-print("test")
+import example
+from time import time
+
+
+def slowFunc():
+    a = 0
+    for x in range(10000000):
+        a+=x
+    return x
+
+
+t = time()
+
+for x in range(10):
+    slowFunc()
+
+q2 = time()-t
+print("normal python:",q2)
+
+t = time()
+
+for x in range(10):
+    example.slowFuncCpp()
+
+q1 = time()-t
+print("cpp accelerated:",q1)
+
+print("speedup:", q2/q1)
+
--- sandbox/test.cpp
+++ sandbox/test.cpp
@@ -1,12 +1,16 @@
 #include <pybind11/pybind11.h>
 namespace py = pybind11;
 
-int add(int i, int j) {
-    return i + j;
+long slowFuncCpp() {
+    long a = 0;
+    for (int i = 0;i<10000000;i++){
+        a+=i;
+    }
+    return a;
 }
 
 PYBIND11_MODULE(example, m) {
 m.doc() = "pybind11 example plugin"; // optional module docstring
 
-m.def("add", &add, "A function which adds two numbers");
+m.def("slowFuncCpp", &slowFuncCpp, "A function which adds two numbers");
 }
